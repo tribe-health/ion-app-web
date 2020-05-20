d7da7dc99116d300ffcb2173e78f65a120426633
Travis James <travis@tribehealthsolutions.com>
2020-05-20 09:53:57 -0500

    specify tribecore turn server

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,11 @@ class App extends React.Component {
     }
 
     console.log("WS url is:" + url);
-    let client = new Client({url: url});
+    let client = new Client({url: url, rtc: { iceServers: [ {
+          credential: "tribecore",
+          username: "tribecore",
+          urls: "turn:coturn.tribecore.io:3478"
+        }]}});
 
     window.onunload = async () => {
       await this._cleanUp();
